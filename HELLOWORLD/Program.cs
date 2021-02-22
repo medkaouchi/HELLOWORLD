a0079de34d42cb0e94d4d428af750dff1781c9b0
kaouchi Mohamed <med_kaouchi@hotmail.com>
2021-02-22 15:39:04 +0100

    gegevens

--- HELLOWORLD/Program.cs
+++ HELLOWORLD/Program.cs
@@ -10,7 +10,19 @@ namespace HELLOWORLD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            Console.WriteLine("Voornaam: ");
+            string Vnaam = Console.ReadLine();
+            Console.WriteLine("Achternaam: ");
+            string Anaam = Console.ReadLine();
+            Console.WriteLine("Straat: ");
+            string Straat = Console.ReadLine();
+            Console.WriteLine("Nummer: ");
+            string Nummer = Console.ReadLine();
+            Console.WriteLine("Stad: ");
+            string Stad = Console.ReadLine();
+            Console.WriteLine("PostCode: ");
+            string PC = Console.ReadLine();
+            Console.WriteLine("naam : {0} {1}\nAdress : {2} {3}, {4} {5}",Vnaam,Anaam,Straat,Nummer,Stad,PC);
             Console.ReadLine();
         }
     }
